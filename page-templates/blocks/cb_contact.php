2126202918642d50166b577ddc4e447719445ef2
Matthew Pont <matthew.pont@gmail.com>
2024-11-14 11:47:19 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -7,14 +7,14 @@
                         <?=get_field('office_address_1','options')?></li>
                     <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-map-marker-alt"></i></span>
                         <?=get_field('office_address_2','options')?></li>
-                    <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span>
+                    <li class="mb-4"><span class="fa-li"><i class="fa-solid fa-phone"></i></span>
                         <a href="tel:<?=get_field('contact_phone','options')?>"><?=get_field('contact_phone','options')?></a></li>
                     <li><span class="fa-li"><i class="fa-solid fa-envelope"></i></span>
                         <a href="mailto:<?=get_field('contact_email','options')?>"><?=get_field('contact_email','options')?></a></li>
                 </ul>
             </div>
             <div class="col-md-6">
-                <?php echo do_shortcode('[gravityform id="1" title="true"]'); ?>
+                <?php echo do_shortcode('[gravityform id="1" title="false"]'); ?>
             </div>
         </div>
     </div>
