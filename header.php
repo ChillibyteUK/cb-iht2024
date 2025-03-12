bcf6ee02fcd1791ccee0df94710e8ba65b256e58
Matthew Pont <matthew.pont@gmail.com>
2025-03-12 16:36:35 +0000

    Update header.php

--- header.php
+++ header.php
@@ -193,13 +193,14 @@ if (!is_user_logged_in()) {
 <header id="wrapper-navbar" class="fixed-top p-0">
     <section class="top_nav d-none d-lg-block py-2">
         <div class="container-xl">
-            <ul class="mb-0">
+            <ul class="mb-0" style="align-items: center;">
                 <li><a href="/about/">About Us</li>
                 <li><a href="/news/">News</li>
                 <li><a href="/contact/">Contact Us</li>
                 <li><?=do_shortcode('[social_fb_icon]')?></li>
                 <li><?=do_shortcode('[social_ig_icon]')?></li>
                 <li><?=do_shortcode('[social_in_icon]')?></li>
+                <li><div class="d-flex"><div class="position-relative"><input type="text" class="form-control rounded-pill search-bar pe-5" placeholder="Search..." aria-label="Search" style="font-size: 0.5rem;"><button class="btn position-absolute top-50 end-0 translate-middle-y" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg></button></div></div></li>
             </ul>
         </div>
     </section>
