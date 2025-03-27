d0b454b60446e69e7a326b9282a7a52f9b006819
Matthew Pont <matthew.pont@gmail.com>
2025-03-27 09:49:52 +0000

    Update cb_nav_cards.php

--- page-templates/blocks/cb_nav_cards.php
+++ page-templates/blocks/cb_nav_cards.php
@@ -20,9 +20,15 @@ $numcards = $count == '4' ? 'col-md-6 col-lg-6' : 'col-md-6 col-lg-4';
                 } else {
                     $nav_cards__link = "";
                 }
+
+                if (str_ends_with($l, ".pdf")) {
+                    $target = "_blank";
+                } else {
+                    $target = "";
+                }
                 ?>
             <div class="<?=$numcards?>" data-aos="fade-up" data-aos-delay="<?=$d?>">
-                <a class="nav_cards__card w-100" href="<?=$l?>">
+                <a class="nav_cards__card w-100" href="<?=$l?>" target="<?=$target?>">
                     <div class="nav_cards__icon bg--<?=$colour?>">
                         <img src="<?=get_sub_field('icon')?>" alt="<?=get_sub_field('title')?>">
                     </div>
