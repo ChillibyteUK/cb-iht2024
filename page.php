e0e5783f383c0f584e1d52a1e4def694bc3b36b1
Matthew Pont <matthew.pont@gmail.com>
2025-04-07 09:58:11 +0100

    Update page.php

--- page.php
+++ page.php
@@ -7,6 +7,21 @@ $theme = get_field('theme') ?: 'blue';
 ?>
 <main id="main" class="theme--<?=$theme?>">
     <?php
+    if ( is_page(275) ) {
+    ?>
+    <section class="hero  hero--short">
+        <img fetchpriority="high" decoding="async" width="1811" height="574" src="/wp-content/uploads/2025/03/Education-Training.jpg" class="hero__bg" alt="Course Calendar" sizes="(max-width: 1811px) 100vw, 1811px">
+        <div class="container-xl py-4">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h1 data-aos="fade-right" class="aos-init aos-animate">Course Calendar</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php
+    }
+    
     the_post();    
     the_content(); 
     ?>
