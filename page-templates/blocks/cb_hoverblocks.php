f4c8610f0e3eeef77e6a3ce8401264672bac1315
Matthew Pont <matthew.pont@gmail.com>
2025-04-15 14:41:25 +0100

    Update cb_hoverblocks.php

--- page-templates/blocks/cb_hoverblocks.php
+++ page-templates/blocks/cb_hoverblocks.php
@@ -1,28 +1,20 @@
-<?php
-$count = 0;
-?>
 <div class="hoverblocks py-5">
     <div class="container-xl">
         <div class="row g-4 justify-content-center">
             <?php
-            $d = 0;
-            while(have_rows('cards')) {
+            while(have_rows('blocks')) {
                 the_row();
                 ?>
-            <div class="<?=$numcards?>" data-aos="fade-up" data-aos-delay="<?=$d?>">
-                <div class="nav_cards__card w-100">
-                    <div class="nav_cards__inner bg--<?=$colour?>">
-                        <h3><?=get_sub_field('title')?></h3>
-                        <div class="nav_cards__content"><?=get_sub_field('content')?></div>
-                        <a href="<?=$l?>" target="<?=$target?>" class="nav_cards__link text-white <?=$nav_cards__link?>"><i class="fa-solid fa-angle-right"></i> Learn more</a>
-                        <?php if (str_ends_with($l, ".pdf")) { ?>
-                        <a href="/courses/" class="nav_cards__link text-white <?=$nav_cards__link?>"><i class="fa-solid fa-angle-right"></i> View available dates</a>
-                        <?php } ?>
-                    </div>
-                </div>
+            <div class="col-md-4 col-lg-6">
+                <?php 
+                $image = get_sub_field('image');
+                if( !empty( $image ) ): ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+                <h3><?=get_sub_field('title')?></h3>
+                 <div class=""><?=get_sub_field('text')?></div>
             </div>
-                <?php
-                $d+=100;
+            <?php
             }
             ?>
         </div>
