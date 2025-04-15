62cd325f7b888b240c1fabd71aafdd4ce0193e75
Matthew Pont <matthew.pont@gmail.com>
2025-04-15 14:56:35 +0100

    Update cb_hoverblocks.php

--- page-templates/blocks/cb_hoverblocks.php
+++ page-templates/blocks/cb_hoverblocks.php
@@ -1,42 +1,48 @@
 <style>
-.hover_container {
-    position: relative;
-    width: 50%;
+/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  border: 1px solid #f1f1f1;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
 }
 
-.image {
-    display: block;
-    width: 100%;
-    height: auto;
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
 }
 
-.overlay {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 100%;
-    width: 100%;
-    opacity: 0;
-    transition: .5s ease;
-    background-color: #008CBA;
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
 }
 
-.hover_container:hover .overlay {
-    opacity: 1;
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
 }
 
-.text {
-    color: white;
-    font-size: 20px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
-    text-align: center;
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
 }
 </style>
 <div class="hoverblocks py-5">
@@ -46,19 +52,19 @@
             while(have_rows('blocks')) {
                 the_row();
                 ?>
-            <div class="card col-md-4 col-lg-4">
-                <?php 
-                $image = get_sub_field('image');
-                if( !empty( $image ) ): ?>
-                    <div class="hover_container">
+            <div class="card flip-card col-md-4 col-lg-4">
+                <div class="flip-card-inner">
+                    <?php 
+                    $image = get_sub_field('image');
+                    if( !empty( $image ) ): ?>
+                    <div class="flip-card-front">
                         <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="image" />
-                        <div class="overlay">
-                            <div class="text"><?=get_sub_field('text')?></div>
-                        </div>
                     </div>
-                <?php endif; ?>
-                <div class="card-body">
-                    <h5 class="card-title"><?=get_sub_field('title')?></h5>
+                    <?php endif; ?>
+                    <div class="flip-card-back">
+                        <h5 class="card-title"><?=get_sub_field('title')?></h5>
+                        <?=get_sub_field('text')?>
+                    </div>
                 </div>
             </div>
             <?php
