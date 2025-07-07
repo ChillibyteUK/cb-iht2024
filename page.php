2fac996013b97857e14389f7973abf3f7093c95b
Matthew Pont <matthew.pont@gmail.com>
2025-07-07 09:49:24 +0100

    Update page.php

--- page.php
+++ page.php
@@ -7,14 +7,14 @@ $theme = get_field('theme') ?: 'blue';
 ?>
 <main id="main" class="theme--<?=$theme?>">
     <?php
-    if ( is_page(275) ) {
+    if ( is_page(275) || has_post_parent(275) ) {
     ?>
     <section class="hero  hero--short">
         <img fetchpriority="high" decoding="async" width="1811" height="574" src="/wp-content/uploads/2025/03/Education-Training.jpg" class="hero__bg" alt="Course Calendar" sizes="(max-width: 1811px) 100vw, 1811px">
         <div class="container-xl py-4">
             <div class="row">
                 <div class="col-lg-8">
-                    <h1 data-aos="fade-right" class="aos-init aos-animate">Course Calendar</h1>
+                    <h1 data-aos="fade-right" class="aos-init aos-animate"><? the_title(); ?></h1>
                 </div>
             </div>
         </div>
